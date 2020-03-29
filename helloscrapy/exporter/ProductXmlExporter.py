a7c5c0c066c9a2e0c789e1250ea9c8dab1b28fca
richasdy <donni.richasdy@gmail.com>
2020-03-29 16:55:41 +0700

    add example exporter

---
+++ helloscrapy/exporter/ProductXmlExporter.py
@@ -0,0 +1,8 @@
+from scrapy.exporter import XmlItemExporter
+
+class ProductXmlExporter(XmlItemExporter):
+
+    def serialize_field(self, field, name, value):
+        if field == 'price':
+            return '$ %s' % str(value)
+        return super(Product, self).serialize_field(field, name, value)
